dcbccbfd2f9f0016ff083e5be89e2e16a56e83c4
Michael Murphy <michael.w.murphy@outlook.com>
2020-03-21 10:18:35 +0000

    Some simple refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
-import './App.scss';
 import TimeInput from './components/TimeInput';
 import TimeOutput from './components/TimeOutput';
+import './App.scss';
 
 class App extends Component {
   constructor() {
     super()
+
     this.state = {
       offset: Math.round(new Date().getTime() / 1000),
       autoUpdate: true
@@ -23,7 +24,12 @@ class App extends Component {
     this.setState({ offset: Math.round(new Date().getTime() / 1000) })
   }
 
+  /**
+   * Attempts to converts the state.offset into a human readable timestamp
+   * using the Unix epoch
+   */
   getUnixDate() {
+    // Number of seconds or nanoseconds since 1970
     const secsDate = new Date(Math.round(this.state.offset * 1000))
     const millDate = new Date(this.state.offset)
 
@@ -33,6 +39,10 @@ class App extends Component {
     ]
   }
 
+  /**
+   * Attempts to converts the state.offset into a human readable timestamp
+   * using the Cocoa epoch
+   */
   getCocoaDate() {
     const cocoaUnixDiff = 978307200 * 1000
 
--- src/components/TimeInput.js
+++ src/components/TimeInput.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './TimeInput.scss';
 
 class TimeInput extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
 
   componentDidMount() {
     setInterval(() => {
